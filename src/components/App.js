c966119e6b70b7ce1de6a5a00cff5b300cff967e
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2024-02-09 23:20:04 +0530

    modified code

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,7 @@
-export const App = () => <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+const App = () => (
+    <div id="main">
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+)
 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById("main"));
+const root = document.getElementById("main");
 
-root.render(<App />)
+ReactDOM.render(<App />, root);
